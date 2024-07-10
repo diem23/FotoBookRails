1d0e3b1d70edbaa400dd9384a47cd1f7e35566e7
diem23 <nmdiem23@gmail.com>
2024-07-11 00:01:33 +0700

    style: complete photos tab of user information

--- app/controllers/personal_info_controller.rb
+++ app/controllers/personal_info_controller.rb
@@ -1,11 +1,15 @@
 class PersonalInfoController < ApplicationController
     def photo
-    #   @target_user= User.find(params[:id])
-    #   @list_photos =  @target_user.photos.where(isPrivate: false).order('photos.created_at DESC')
+      @target_user= User.find(params[:id])
+      puts params[:id]
+      puts @target_user
+      @list_photos =  @target_user.photos.where(isPrivate: false).order('photos.created_at DESC')
     end
 
     def album
       @target_user= User.find(params[:id])
+      puts params[:id]
+      puts @target_user
       @list_albums = @target_user.albums.where(isPrivate: false).order('albums.created_at DESC')
     end
     def following
